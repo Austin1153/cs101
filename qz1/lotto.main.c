5b5b6ca03231b413a3a07f0ff4c2bc26b62ab454
B1029048.mac <B1029048@cgu.edu.tw>
2022-03-09 20:35:35 +0800

    qz1

--- qz1/lotto.main.c
+++ qz1/lotto.main.c
@@ -2,6 +2,31 @@
 #include <stdlib.h>
 #include <time.h>
 
+void namef(char name[],int c) {
+    for (int i = 8; i >= 5; i--,c /= 10) {
+        name[i] = (c%10+'0');
+    }
+}
+
+int countf() {
+    int n;
+    FILE *fp;
+    if ((fp = fopen("count.bin","rb+")) == NULL) {
+        n = 0;
+        fp = fopen("count.bin","wb");
+        fwrite(&n,sizeof(int),1,fp);
+        fclose(fp);
+    }
+    fp = fopen("count.bin","rb+");
+    fread(&n,sizeof(int),1,fp);
+    fclose(fp);
+
+    n = n + 1;
+    fp = fopen("count.bin","wb+");
+    fwrite(&n,sizeof(int),1,fp);
+    return n;
+}
+
 void sort_arr(int lotto[]) {
     int tmp = 70, tmpn = 0;
     for (int i = 0; i <= 5; i++) {
@@ -44,13 +69,17 @@ int main() {
     int n;
     scanf("%d",&n);
     
+    int count = countf();
+    char name[10]="lotto";
+    namef(name,count);
     time_t currtime = time(&currtime);
     srand(time(NULL));
     int lotto[7];
     
     FILE *fp;
-    fp = fopen("lotto.txt","w+");
+    fp = fopen(name,"w+");
     fprintf(fp,"======== lotto048 ========\n");
+    fprintf(fp,"========= %0*d =========\n",6,count);
     fprintf(fp," %s",ctime(&currtime));
     for (int i = 1; i <= 5; i++) {
         fprintf(fp,"[%d]:",i);
