558176ec807010657c9e004176aea1493c6bf689
B1029048.mac <B1029048@cgu.edu.tw>
2022-05-06 10:39:56 +0800

    QZ#5 final

--- qz5/main.c
+++ qz5/main.c
@@ -13,28 +13,38 @@ node_t* allocate_node(int data) {
 }
 
 void show_list(node_t* list) {
-    while (list->head) {
+    while (list->next) {
         printf("[%d]->", list->head);
         list = list->next;
     }
-    printf("null");
+    printf("[%d]->", list->head);
+    printf("null\n");
 }
 
 node_t* append_node(node_t* list, int new_data) {
     node_t* tmp = list;
+    if (list == NULL) {
+        list = allocate_node(new_data);
+        return list;
+    }
     while (tmp->next != NULL) {
         tmp = tmp->next;
     }
-    if (*(tmp->head) == NULL) {
-        tmp->head = new_data;
-    } else {
-        tmp->next = allocate_node(new_data);
-    }
+    tmp->next = allocate_node(new_data);
     return list;
 }
 
 void free_all_node(node_t* list) {
-
+    node_t* tmp;
+    while(list->next) {
+        tmp = list->next;
+        printf("free([%d])->", list->head);
+        free(list);
+        list = tmp;
+    }
+    printf("free([%d])->", list->head);
+    free(list);
+    printf("null\n");
 }
 
 int main() {
@@ -47,6 +57,6 @@ int main() {
     show_list(head);
     head = append_node(head, 3333);
     show_list(head);
-    // free_all_node(head);
+    free_all_node(head);
     return 0;
 }
